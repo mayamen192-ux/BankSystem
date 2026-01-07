e0ff2efb7f6b69a6efc917068e4d60870b7cc0e5
Mayamin.sh <Mayamin.sh@MEMLT0040>
2026-01-07 12:18:09 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace BankSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] owners = new string[100];
+            string[] accounts = new string[100];
+            double[] balances = new double[100];
+            int LastAccountIndex = -1;
+            while (true)
+            {
+                Console.WriteLine("Welcome to the Bank System");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. Check Balance");
+                Console.WriteLine("5. Transfer Money");
+                Console.WriteLine("6. Exit");
+                Console.Write("Please select an option: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        //Phase2: input &  //Phase3: processing
+                        Console.Write("Enter your name: ");
+                        owners[LastAccountIndex + 1] = Console.ReadLine();
+                        Console.Write("Enter initial deposit amount: ");
+                        balances[LastAccountIndex + 1] = double.Parse(Console.ReadLine());
+                        accounts[LastAccountIndex + 1] = "ACC" + (LastAccountIndex + 1); //processing order
+
+                        //Phase4: output
+                        Console.WriteLine("Account created successfully!");
+                        Console.WriteLine("Account Number: " + accounts[LastAccountIndex + 1]);
+
+                        LastAccountIndex++;
+                        break;
+                    case 2:
+                        break;
+
+
+
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    default:
+                        Console.WriteLine("Invaild account number ");
+                        break;
+                }
+                Console.WriteLine("Thank you for using the Bank System, press any key");
+                Console.ReadLine();
+                Console.Clear();
+
+            }
+        }
+        }
+    }
